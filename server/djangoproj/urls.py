601a86cefab6d87b0751d1012d39e0cfc7f3e712
lettauaugustowalter <lettauaugustowalter@gmail.com>
2026-08-27 15:52:01 -0400

    Added Contact Us page

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -12,4 +12,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', TemplateView.as_view(template_name="About.html")),
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
