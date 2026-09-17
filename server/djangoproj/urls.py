4cd2f9f7e2a35522d4dbcf470ca0f018c6317b89
Walter Lettau <lettauaugustowalter@gmail.com>
2026-09-17 16:43:22 -0400

    Implement dealer details, reviews, and review submission

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,8 +1,3 @@
-"""djangoproj URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -11,6 +6,13 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('djangoapp/', include('djangoapp.urls')),
+    path('', TemplateView.as_view(template_name="Home.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
+    path('postreview/<int:dealer_id>', TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
