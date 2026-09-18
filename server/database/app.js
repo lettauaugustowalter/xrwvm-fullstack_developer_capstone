cb78e40c82200ab9af0c046a5326de30b2548ea4
lettauaugustowalter <lettauaugustowalter@gmail.com>
2026-09-18 15:03:16 -0400

    Resolve all JS and Python linting issues

--- server/database/app.js
+++ server/database/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const  cors = require('cors')
+const cors = require('cors');
 const app = express();
 const port = 3030;
 
@@ -12,17 +12,17 @@ app.use(require('body-parser').urlencoded({ extended: false }));
 const reviews_data = JSON.parse(fs.readFileSync("data/reviews.json", 'utf8'));
 const dealerships_data = JSON.parse(fs.readFileSync("data/dealerships.json", 'utf8'));
 
-mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
+mongoose.connect("mongodb://mongo_db:27017/", { dbName: 'dealershipsDB' });
 
 const Reviews = require('./review');
 const Dealerships = require('./dealership');
 
 try {
-  Reviews.deleteMany({}).then(()=>{
-    Reviews.insertMany(reviews_data['reviews']);
+  Reviews.deleteMany({}).then(() => {
+    Reviews.insertMany(reviews_data.reviews);
   });
-  Dealerships.deleteMany({}).then(()=>{
-    Dealerships.insertMany(dealerships_data['dealerships']);
+  Dealerships.deleteMany({}).then(() => {
+    Dealerships.insertMany(dealerships_data.dealerships);
   });
 } catch (error) {
   console.error('Error seeding data:', error);
@@ -30,7 +30,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose Database");
+  res.send("Welcome to the Mongoose Database");
 });
 
 // Express route to fetch all reviews
@@ -46,14 +46,14 @@ app.get('/fetchReviews', async (req, res) => {
 // Express route to fetch reviews by a particular dealer
 app.get('/fetchReviews/dealer/:id', async (req, res) => {
   try {
-    const documents = await Reviews.find({dealership: req.params.id});
+    const documents = await Reviews.find({ dealership: req.params.id });
     res.json(documents);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching documents' });
   }
 });
 
-// Express route to fetch all dealerships
+// Express route to fetch all dealers
 app.get('/fetchDealers', async (req, res) => {
   try {
     const documents = await Dealerships.find();
@@ -66,7 +66,7 @@ app.get('/fetchDealers', async (req, res) => {
 // Express route to fetch dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
   try {
-    const documents = await Dealerships.find({state: req.params.state});
+    const documents = await Dealerships.find({ state: req.params.state });
     res.json(documents);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching documents' });
@@ -76,7 +76,7 @@ app.get('/fetchDealers/:state', async (req, res) => {
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
   try {
-    const documents = await Dealerships.find({id: req.params.id});
+    const documents = await Dealerships.find({ id: req.params.id });
     res.json(documents);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching documents' });
@@ -87,18 +87,18 @@ app.get('/fetchDealer/:id', async (req, res) => {
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   let data = JSON.parse(req.body);
   const documents = await Reviews.find().sort({ id: -1 });
-  let new_id = documents.length > 0 ? documents[0]['id'] + 1 : 1;
+  let new_id = documents.length > 0 ? documents[0].id + 1 : 1;
 
   const review = new Reviews({
     "id": new_id,
-    "name": data['name'],
-    "dealership": data['dealership'],
-    "review": data['review'],
-    "purchase": data['purchase'],
-    "purchase_date": data['purchase_date'],
-    "car_make": data['car_make'],
-    "car_model": data['car_model'],
-    "car_year": data['car_year'],
+    "name": data.name,
+    "dealership": data.dealership,
+    "review": data.review,
+    "purchase": data.purchase,
+    "purchase_date": data.purchase_date,
+    "car_make": data.car_make,
+    "car_model": data.car_model,
+    "car_year": data.car_year,
   });
 
   try {
